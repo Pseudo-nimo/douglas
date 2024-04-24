6274d2036366ff140e8e6f605ba05ecc5dee50c4
Paulino PJ <paulinopereirajr@gmail.com>
2024-04-24 17:25:37 -0300

    .

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import math
 # Mapa
 tabuleiro = list()
 
-_initialPos = (0,0)
+_initialPos = [0,0]
 
-finalPos = (6,7)
+finalPos = [6,7]
 
 _movementModes = [
     'Vazio','diagonal','vertical','horizontal'
 ]
+
+
+
+
 
 class Player():
     h =1
@@ -17,20 +21,52 @@ class Player():
     d = 1
     def __init__(self):
         self.Pos = _initialPos
-    def analisis(self):
+
+    def analisis(self)->list:
+
         #checar diagonal
         if (self.d==0): 
             # o robo deve escolher andar pelo menor cateto
-            pass
+
+            print('d=0')
+
+            if self.checarCatetos(self.Pos) == 0:
+                if (self.h != 0):
+                    nextDirection = [1,0]
+                    pass
+
+            elif self.checarCatetos(self.Pos) == 1:
+                if (self.h != 0):
+                    nextDirection = [0,1]
+                    pass
+            
+            else: 
+                if (self.h != 0):
+                    nextDirection = [1,0]
+                    pass
+
+
         else:
-            nextDirection = (1,1)
+            #se a diagonal nao estiver bloqueada, devemos andar por ela
+            nextDirection = [1,1]
         return nextDirection
 
     def walk(self):
-        self.Pos[0] += self.analisis[0]
-        self.Pos[1] += self.analisis[1]
+        self.Pos[0] += self.analisis[0] # type: ignore
+        self.Pos[1] += self.analisis[1] # type: ignore
         
         pass
+
+    def checarCatetos(self, pos: list):
+    
+        hDistance = finalPos[0] - pos[0]
+        vDistance = finalPos[1] - pos[1]
+
+        if hDistance>vDistance:
+            return 0
+        elif hDistance<vDistance:
+            return 1
+        else: return 2
         
             
 
