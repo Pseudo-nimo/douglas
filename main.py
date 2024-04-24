522e69dbf5a5fdc52d72fb824b32107b3ef870ae
Paulino PJ <paulinopereirajr@gmail.com>
2024-04-24 14:03:27 -0300

    inicio de programação, definindo o ambiente

---
+++ main.py
@@ -0,0 +1,27 @@
+import math
+
+# Mapa
+tabuleiro = list()
+
+_initialPos = (0,0)
+
+finalPos = (6,7)
+
+_movementModes = [
+    'Vazio','diagonal','vertical','horizontal'
+]
+
+class Player():
+    h,v,d = 1
+    def __init__(self):
+        Pos = _initialPos
+    def analisis():
+        if not (d==0): return 0
+
+    def walk():
+        pass
+        
+            
+
+if __name__() == __init__():
+    robot = Player()
