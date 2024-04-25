116a353edd8f64835d75f8eaebfab39af8613111
Paulino PJ <paulinopereirajr@gmail.com>
2024-04-25 15:32:17 -0300

    .

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import math
 import time
 
@@ -7,6 +8,7 @@ finalPos = [6,7]
 _movementModes = [
     'Vazio','diagonal','vertical','horizontal'
 ]
+
 
 
 class Space():
@@ -30,62 +32,73 @@ class finalSpace(Space):
 class MoveRestriction(Space):
     def __init__(self):
         self.r = 1
-        
+
+
+
 
 
 class Player():
     h: Space
     v: Space
     d: Space
-
-    
+    Pos: list
     # 100% do código até agora foi escrito a mão
-
 
     def __init__(self, t):
         self.Pos = _initialPos
+        self.d=tabuleiro[self.Pos[0]+1][self.Pos[1]+1]
+        self.h=tabuleiro[self.Pos[0]+1][self.Pos[1]]
+        self.v=tabuleiro[self.Pos[0]][self.Pos[1]+1]
+        
         self.tab = t
 
+
+
     def observarArredores(self):
-        global d,  v, h
-        d=tabuleiro[self.Pos[0]+1][self.Pos[1]+1]
+        global d, v, h, Pos
+        self.d=tabuleiro[self.Pos[0]+1][self.Pos[1]+1]
+        self.h=tabuleiro[self.Pos[0]+1][self.Pos[1]]
+        self.v=tabuleiro[self.Pos[0]][self.Pos[1]+1]
         return 0
 
-    def analysis(self)->list:
-        
+    def analysis(self) -> list:
+        global d, v, h
         #checar diagonal
-        nextDirection=[0,0]
+        nextDirection = [-1,-1]
         self.observarArredores()
         checking = self.checarCatetos(self.Pos)
 
         if (checking > 2) : 
             # o robo deve checar se já está alinhado com o objetivo
             if checking == 3:
-                if (self.h != 0):
+                if (self.h.r != 0):
                     nextDirection = [1,0]
                     pass
             elif checking == 1:
-                if (self.v != 0):
+                if (self.v.r != 0):
                     nextDirection = [0,1]
                     pass
+            if checking == 12:
+                nextDirection = [0,0]
 
-        if (self.d==0): 
+
+        if not (self.d.r == 255): 
             # o robo deve escolher andar pelo menor cateto
 
-            print('d=0')
+            print('nao pode diagonalizar')
 
             if checking == 0:
-                if (self.h != 0):
+                if (self.h.r != 0):
                     nextDirection = [1,0]
                     pass
                         
             elif checking == 1:
-                if (self.v != 0):
+                if (self.v.r != 0):
                     nextDirection = [0,1]
                     pass
             
             elif checking == 2: 
-                if (self.h != 0):
+                if (self.h.r != 0):
                     nextDirection = [1,0]
                     pass
 
@@ -93,14 +106,18 @@ class Player():
         else:
             #se a diagonal nao estiver bloqueada, devemos andar por ela
             nextDirection = [1,1]
+
         return nextDirection
         
 
     def walk(self):
         time.sleep(2)
-        self.Pos[0] += self.analysis[0] # type: ignore
-        self.Pos[1] += self.analysis[1] # type: ignore
-        
+        if self.Pos == finalPos:
+            return 12
+        else:
+            self.Pos[0] = self.Pos[0] + self.analysis()[0]
+            self.Pos[1] += self.analysis()[1] 
+            
         return -1
 
     def checarCatetos(self, pos: list)-> int:
@@ -117,15 +134,13 @@ class Player():
             else: 
                 return 2
         else: 
+
             if   hDistance == 0:
                 return 3
             if   vDistance == 0:
                 return 4
         return -1
-            
 
-
-            
 
 if __name__ == '__main__':
     
@@ -136,6 +151,7 @@ if __name__ == '__main__':
     tabuleiro[_initialPos[0]][_initialPos[1]] = initialSpace()
     tabuleiro[finalPos[0]][finalPos[1]] = finalSpace()
     robot = Player(tabuleiro)
+    tabuleiro[1][1] = MoveRestriction()
     Gameloop = True
 
     for i in range(9,-1, -1):
@@ -145,8 +161,9 @@ if __name__ == '__main__':
 
         print()
 
-    '''
+    
     while Gameloop:
-        
-        pass
-'''
+        print(robot.Pos)
+        if robot.walk() == 12:
+            print('chegou')
+            Gameloop=False
