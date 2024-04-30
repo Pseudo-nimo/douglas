c2890da72a980ddee1c9c5060ca62c2adde7ab31
Paulino PJ <paulinopereirajr@gmail.com>
2024-04-30 13:42:40 -0300

    limpando código e iniciando as baterias

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import os
-import math
-import time
+from time import sleep 
 
 # Mapa
 _initialPos = [0,0]
-finalPos = [3,9]
+finalPos = [9,1]
 _movementModes = [
     'Vazio','diagonal','vertical','horizontal'
 ]
@@ -26,6 +24,7 @@ class Camp():
     end: Space
 
     def __init__(self, i:list, e):
+        
         self.tabuleiro= [[Space([i,j]) for i in range(11)] for j in range(11)]
         self.init= Space(i)
         self.end=Space(e)
@@ -48,23 +47,25 @@ class MoveRestriction(Space):
         self.r = 1
 
 
-
-
-
 class Player():
     h: Space
     v: Space
     d: Space
     Pos: list
     tab: Camp
+    batery: int
     # 100% do código até agora foi escrito a mão
 
     def __init__(self, t):
         self.Pos = _initialPos
         self.tab = t
-        '''self.d= self.tab.tabuleiro[self.Pos[0]+1][self.Pos[1]+1]
-        self.h= self.tab.tabuleiro[self.Pos[0]+1][self.Pos[1]]
-        self.v= self.tab.tabuleiro[self.Pos[0]][self.Pos[1]+1]'''
+        self.batery = 4
+
+    def recharge(self):
+        for i in range (4):
+            print('recarregando: ', self.batery)
+            sleep(1)
+
 
     def observarArredores(self):
         global d, v, h, Pos, tab
@@ -77,28 +78,27 @@ class Player():
         global d, v, h
 
         #checar diagonal
-        nextDirection = [-1,-1]
+        
         self.observarArredores()
-        checking = self.checarCatetos()
 
+        vDistance = self.tab.end.Pos[1] - self.Pos[1]
+        hDistance = self.tab.end.Pos[0] - self.Pos[0]
+
+        '''checking = self.checarCatetos()'''
 
         if not (self.d.r == 255): 
-
-            if checking == 0:
+            if hDistance > vDistance:
                 if (self.h.r != 0):
                     return [1,0]
-                    
-                        
-            elif checking == 1:
+                            
+            elif hDistance < vDistance:
                 if (self.v.r != 0):
                     return [0,1]
                     
-            
-            elif checking == 2: 
+            else: 
                 if (self.h.r != 0):
                     nextDirection = [1,0]
                     pass
-            else: nextDirection=[0,0]
         else:
             #se a diagonal nao estiver bloqueada, devemos andar por ela
             nextDirection = [1,1]
@@ -107,7 +107,7 @@ class Player():
         
 
     def walk(self):
-        time.sleep(0.5)
+        sleep(0.5)
         results = self.analysis()
         if self.Pos == self.tab.end.Pos:
             return 12
@@ -120,20 +120,6 @@ class Player():
             self.Pos[1] = self.Pos[1] + results[1]
             
             
-        return -1
-
-    def checarCatetos(self)-> int:
-    
-        vDistance = self.tab.end.Pos[1] - self.Pos[1]
-        hDistance = self.tab.end.Pos[0] - self.Pos[0]
-
-
-        if   hDistance > vDistance:
-            return 0
-        elif hDistance < vDistance:
-            return 1
-        else: 
-            return 2
         return -1
 
 
@@ -149,7 +135,6 @@ if __name__ == '__main__':
     for i in range(9,-1, -1):
         for j in range(10):
             print(f'[{campo.tabuleiro[j][i].r:^5}]', end = '')
-
         print()
 
     
