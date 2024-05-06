da3da4c4be5a2c9849be8fd4448b646c37ebf37e
Paulino PJ <paulinopereirajr@gmail.com>
2024-05-06 11:48:16 -0300

    editando os minérios

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from time import sleep 
 from enum import Enum
 
+TESTING = 0
+
 # Mapa
-INITIAL_POS = [0,0]
-FINAL_POS = [6,6]
+INITIAL_POS = (0,0)
+FINAL_POS = (7,6)
 Gameloop = True
 
 class Content(Enum):
@@ -21,8 +23,8 @@ class Content(Enum):
 class Space():
     r:int
     Pos:list
-    def __init__(self, p: list):
-        self.Pos = p
+    def __init__(self, x,y):
+        self.Pos = [x,y]
         self.content = Content.EMPTY
         pass
 
@@ -32,75 +34,79 @@ class Camp():
     end: Space
 
     def __init__(self):
+        self.matrix= [[Space(*(column,line)) for line in range(11)] for column in range(11)]
         
-        self.matrix= [[Space([column,line]) for line in range(11)] for column in range(11)]
-        
-    
     def createSpace(self,spaceType:Space):
         self.matrix[spaceType.Pos[0]][spaceType.Pos[1]] = spaceType
         if spaceType.content == Content.FINAL_SPACE:
             self.end = spaceType
+        
+            
 
-    def getSpacebyPosition(self,p):
-        return self.matrix[p[0]][p[1]]
+    def getSpace(self,x,y)->Space:
+        return self.matrix[x][y]
 
 class initialSpace(Space):
-    def __init__(self,pos):
+    def __init__(self,x,y):
+        self.Pos=[x,y]
         self.content=Content.INITIAL_SPACE
-        self.Pos=pos
+        
 
 class finalSpace(Space):
-    def __init__(self,p):
+    def __init__(self, x,y):
         self.content=Content.FINAL_SPACE
-        self.Pos=p
+        self.Pos= [x,y]
 
 class MoveRestriction(Space):
-    def __init__(self,pos):
+    def __init__(self, x,y):
+        self.Pos=[x,y]
         self.content = Content.MOVE_RESTRICTION
-        self.Pos=pos
+        
 
 class RechargeRestriction(Space):
-    def __init__(self,pos):
+    def __init__(self, x,y):
+        self.Pos=[x,y]
         self.content = Content.RECHARGE_RESTRICTION
-        self.Pos=pos
 
-class Mineral(Space):
-    def __init__(self,p):
-        pass
 
-class Gold(Mineral):
-    def __init__(self, p):
-        self.Pos=p
+class Gold(Space):
+    def __init__(self, x,y):
+        self.Pos=[x,y]
         self.content=Content.GOLD
+        
 
-class Silver(Mineral):
-    def __init__(self, p):
-        self.Pos=p
+class Silver(Space):
+    def __init__(self, x,y):
+        self.Pos=[x,y]
         self.content=Content.SILVER
 
-class Player():
+class Bronze(Space):
+    def __init__(self, x,y):
+        self.Pos=[x,y]
+        self.content=Content.BRONZE
 
-    def __init__(self, t):
-        self.position = [0,0]
-        self.world = t
+class Player():
+    def __init__(self, board):
+        
+        self.world = board
+        self.position = INITIAL_POS
         self.batery=[1,1,1,1]
 
     def recharge(self):
         while len(self.batery)<5:
             self.batery.append(1)
-            sleep(1)
+            sleep(1* TESTING)
 
     def move(self):
-        sleep(1)
+        sleep(2 * TESTING)
         results:Space
         # a variavel results deve guardar as informações do espaço escolhido para ser o próximo
 
-        self.diagonal = self.world.matrix [self.position[0]+1] [self.position[1]+1]
-        self.diagonal = self.world.getSpacebyPosition( [self.position[0]+1, self.position[1]+1])
-        
-        self.horizontal= self.world.matrix[self.position[0]+1][self.position[1]]
-
-        self.vertical= self.world.matrix[self.position[0]][self.position[1]+1]
+        #self.diagonal = self.world.matrix [self.position[0]+1] [self.position[1]+1]
+        _p = self.position
+        self.diagonal = self.world.getSpace( _p[0]+1, _p[1]+1)
+        self.horizontal = self.world.getSpace( _p[0]+1, _p[1])
+        self.vertical= self.world.getSpace( _p[0], _p[1]+1)
         
         #se a diagonal nao estiver bloqueada, devemos andar por ela
         if (self.diagonal.content != Content.MOVE_RESTRICTION): results= self.diagonal
@@ -126,7 +132,7 @@ class Player():
         
         # Cancela o movimento diagonal quando alinhar
         if (FINAL_POS[0]-self.position[0])==0:  results = self.vertical
-        elif(FINAL_POS[1]-self.position[1])==0: results=self.horizontal 
+        elif(FINAL_POS[1]-self.position[1])==0: results = self.horizontal 
 
         # Recarga impossivel no proximo espaço
         shadowZone = results.content == Content.RECHARGE_RESTRICTION
@@ -142,12 +148,19 @@ if __name__ == '__main__':
     campo = Camp()
     robot = Player(campo)
     
-    campo.createSpace(MoveRestriction([2,2]))
-    campo.createSpace(MoveRestriction([3,2]))
-    campo.createSpace(MoveRestriction([5,3]))
-    campo.createSpace(RechargeRestriction([6,3]))
-    campo.createSpace(initialSpace(INITIAL_POS))
-    campo.createSpace(finalSpace(FINAL_POS))
+    campo.createSpace(MoveRestriction(2,2))
+    campo.createSpace(MoveRestriction(3,2))
+    campo.createSpace(MoveRestriction(5,3))
+    campo.createSpace(MoveRestriction(1,2))
+    campo.createSpace(RechargeRestriction(6,3))
+    campo.createSpace(Gold(1,1))
+    campo.createSpace(Silver(3,1))
+    campo.createSpace(Silver(7,4))
+    campo.createSpace(Bronze(4,2))
+    campo.createSpace(Bronze(7,5))
+    campo.createSpace(initialSpace(*INITIAL_POS))
+    campo.createSpace(finalSpace(*FINAL_POS))
+    
     
 
     for line in range(9,-1, -1):
@@ -155,10 +168,18 @@ if __name__ == '__main__':
             print(f'[{campo.matrix[column][line].content.value :^5}]', end = '')
         print()
 
-    
     while Gameloop:
         robot.move()
+        if(campo.getSpace(*robot.position).content == Content.GOLD):
+            print('OUROOOOOOOOOOOOOOOOOOO')
+        if(campo.getSpace(*robot.position).content == Content.SILVER):
+            print('PRATAAAAAAAAAAAAAAAAAA')
+        if(campo.getSpace(*robot.position).content == Content.BRONZE):
+            print('BRONZEEEEEEEEEEEEEEEE')
+            
+            
+            
         print(robot.position)
-        if robot.position == FINAL_POS:
+        if robot.position == list(FINAL_POS):
             print('chegou')
-            Gameloop=False
+            Gameloop = False
